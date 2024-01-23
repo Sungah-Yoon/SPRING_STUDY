1c9ac1cbb5fd3d7cd7b92b80ab09ff35405421da
Sungah <sungah0815@gmail.com>
2024-01-23 17:05:24 +0900

    test: 싱글톤 컨테이너 테스트

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: 사용자 A가 10000원을 주문함
+        int userAPrice = statefulService1.order("userA", 10000);
+        // ThreadB: 사용자 B가 20000원을 주문함
+        int userBPrice = statefulService2.order("userB", 20000);
+        
+        // ThreadA: 사용자 A가 주문 금액을 조회함
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
